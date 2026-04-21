d06c2578ae97255f95925ed534af94121c3e9980
Edi <poqueque@yahoo.es>
2026-04-21 12:39:24 +0200

    After Session 2

---
+++ android/app/src/main/kotlin/net/poquesoft/harry_potter/MainActivity.kt
@@ -0,0 +1,5 @@
+package net.poquesoft.harry_potter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static TextStyle potterStyle = TextStyle(fontSize: 24);
+  static TextStyle potterStyleBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+}
